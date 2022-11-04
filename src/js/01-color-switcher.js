11c33ecbf19c34f7ff58e331e522d959827da4aa
IrynaHavryliuk <99551348+IrynaHavryliuk@users.noreply.github.com>
2022-11-04 23:18:28 +0200

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,39 +1,39 @@
-BtnStartEl = document.querySelector('button[data-start]');
-BtnStopEl = document.querySelector('button[data-stop]');
-const ACTION_DELAY = 1000;
-const onInretval = {
-  timerId: null,
-  isActiveBtn: false,
-};
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyBox = document.querySelector('body');
 
-BtnStartEl.addEventListener('click', onStart);
-BtnStopEl.addEventListener('click', onStop);
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+let intervalId = null;
+
+startBtn.style.padding = '10px';
+stopBtn.style.padding = '10px';
+
+
+function onStartClick() {
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+       intervalId = setInterval(() => {
+         const colorOfRandom = getRandomHexColor();
+         bodyBox.style.backgroundColor = `${colorOfRandom}`;
+         
+       }, 1000);
+    
+};
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
 
-function onStart() {
-  if (onInretval.isActiveBtn) {
-    return;
-  }
-  onInretval.isActiveBtn = true;
-  BtnStartEl.setAttribute('disabled', true);
-  BtnStopEl.removeAttribute('disabled');
-  onInretval.timerId = setInterval(() => {
-    document.body.style.backgroundColor = getRandomHexColor();
-  }, ACTION_DELAY);
-  console.log('onStart');
-}
 
-function onStop() {
-  if (BtnStartEl.hasAttribute('disabled')) {
-    BtnStopEl.setAttribute('disabled', true);
-    BtnStartEl.removeAttribute('disabled');
-    clearInterval(onInretval.timerId);
-    onInretval.isActiveBtn = false;
-  } else {
-    return;
-  }
-  console.log('onStop');
+function onStopClick() {
+
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    
+    clearInterval(intervalId);
 }
